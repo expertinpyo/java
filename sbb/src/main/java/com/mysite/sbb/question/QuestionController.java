9bfc02bcb98d6c467730cfebdd0fc994714700b4
expertinpyo <ypd02363@naver.com>
2022-07-08 02:02:29 +0900

    java spring boot practice, finished 2-10

---
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,43 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+	// Service 사용 이유 : 모듈화의 이점, 보안 문제 때문에 사용 권장됨
+	// 여기선 현재 구현되어 있지 않으나 Entity class 대신 DTO(Data Transfer Object) 클래스 필요.
+	// 실제 업무에선 Entity 클래스를 대신할 DTO 클래스를 만들어 사용한다.
+	// Controller들은 직접 Repository를 거치지 않고 Controller => Service => Repository 구조로
+	// 사용하면 된다.
+//	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
+
+//	@RequestMapping("/question/list")
+	@RequestMapping("/list")
+
+	public String list(Model model) {
+//		List<Question> questionList = this.questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
+		model.addAttribute("questionList", questionList);
+		return "question_list";
+	}
+
+//	@RequestMapping(value = "/question/detail/{id}")
+	@RequestMapping(value = "/detail{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
+	}
+}
+
--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,27 @@
 package com.mysite.sbb;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired // 객체 주입을 위해 사용하는 에너테이션. setter와 같은 역할. 테스트 코드 작성시에만 Autowired 사용하는 듯.
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb란?");
+		q1.setContent("누가 제발 알려줘");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
 	}
 
 }
